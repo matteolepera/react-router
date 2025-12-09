e25f8ca763ba1f7fe7daa456e47a22785bfc2bc5
matteolpera <matteo.lepera02@gmail.com>
2025-12-09 14:17:01 +0100

    feat: wip header: aggiunto array di oggetti per i link della nav bar, fatto map per aggiungere i link.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,59 @@
+import { Link, NavLink } from "react-router-dom";
+import logo from "../assets/image/logo.png"
+
 export default function Header() {
+    const navBarLinks = [
+        {
+            id: 1,
+            name: "Home",
+            path: "/"
+        },
+        {
+            id: 2,
+            name: "Prodotti",
+            path: "/prodotti"
+        },
+        {
+            id: 3,
+            name: "Chi Siamo",
+            path: "/chi-siamo"
+        },
+        {
+            id: 4,
+            name: "Contatti",
+            path: "/contatti"
+        }
+    ];
+
     return (
-        <header>sono header</header>
+        <header>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                        <Link className="navbar-brand" to={"/"}>
+                            <img src={logo} alt="logo buy 'n bleed" width="100" height="50" />
+                        </Link>
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            {navBarLinks.map((link) =>
+                                <li key={link.id} className="nav-item">
+                                    <NavLink className="nav-link" aria-current="page" to={link.path}>{link.name}</NavLink>
+                                </li>
+                            )}
+                        </ul>
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/login">Account</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/login">carrello</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
     )
 }
