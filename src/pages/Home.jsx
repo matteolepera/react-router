f053fd60dbd513399e433c9b93066786e03bec40
matteolpera <matteo.lepera02@gmail.com>
2025-12-09 16:33:40 +0100

    feat: lavorato sulla home, aggiunto hero banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
     return (
-        <h1>home</h1>
+        <div className="hero">
+            <div className="hero-content">
+                <h1>Regala Dolore… Risparmia Sangue!</h1>
+                <p>Sconti natalizi così forti che faranno piangere il tuo portafoglio. Shoppa prima che sia troppo tardi!</p>
+                <Link className="btn btn-danger" to={"/prodotti"}>Dolore in Offerta</Link>
+            </div>
+        </div>
     )
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -26,8 +26,7 @@ export default function Products() {
                             <img src={product.image} className="card-img-top" alt={product.title} />
                             <div className="card-body">
                                 <h5 className="card-title">{product.title}</h5>
-                                <p className="card-text">{product.description}</p>
-                                <a href="#" className="btn btn-primary">Aggiungi al carrello</a>
+                                <p>{product.price}&euro;</p>
                             </div>
                         </div>
                     </div>
